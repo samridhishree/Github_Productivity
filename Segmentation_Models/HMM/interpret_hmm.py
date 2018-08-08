062810aa3ac5fd423f870c61bec09e693d925778
samridhishree <sami@samridhishree.com>
2018-08-08 15:02:06 -0400

    Fixing the folder name

--- Segmentation_Models/HMM/interpret_hmm.py
+++ Segmentation_Models/HMM/interpret_hmm.py
@@ -11,7 +11,7 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model_dir', help='The trained models directory', default='Sample_Data/HMM/results/GaussianHMMdiag/')
-parser.add_argument('--output_dir', help='Interpreted csv output folder', default='Sample_Data/HMM/result/model_interpretations/')
+parser.add_argument('--output_dir', help='Interpreted csv output folder', default='Sample_Data/HMM/results/model_interpretations/')
 args, unknown = parser.parse_known_args()
 
 
